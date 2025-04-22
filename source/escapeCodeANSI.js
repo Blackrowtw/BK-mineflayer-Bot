a63b5c63415c67496b02e8e52eae1257854997a3
Blacktowtw <blackrow.013@gmail.com>
2025-04-23 00:46:23 +0800

    更新命令行顏色代碼: 新增顏色

--- source/escapeCodeANSI.js
+++ source/escapeCodeANSI.js
@@ -3,8 +3,11 @@ const BOLD_RED = "\x1b[31;1m";
 const BOLD_SKY = "\x1b[36;1m";
 const BOLD_MAGENTA = "\x1b[35;1m";
 const DARK_RED = "\x1b[38;5;9m";
+const GOLD = "\x1b[38;5;220m";
 const SKY = "\x1b[36m";
-const YELLOW = "\x1b[38;5;220m";
+const GREEN = "\x1b[32m";
+const YELLOW = "\x1b[33m";
+const RED = "\x1b[31m";
 const BLACK = "\x1b[38;5;8m";
 const ITALIC_BLACK = "\x1b[30;3m";
 const REVERSE = "\x1b[7m";
@@ -17,8 +20,11 @@ const REVERSE_RED = "\x1b[31;7m";
 //     BOLD_SKY,
 //     BOLD_MAGENTA,
 //     DARK_RED,
+//     GOLD,
 //     SKY,
+//     GREEN,
 //     YELLOW,
+//     RED,
 //     BLACK,
 //     ITALIC_BLACK,
 //     REVERSE,
@@ -31,8 +37,11 @@ module.exports = {
   BOLD_SKY,
   BOLD_MAGENTA,
   DARK_RED,
+  GOLD,
   SKY,
+  GREEN,
   YELLOW,
+  RED,
   BLACK,
   ITALIC_BLACK,
   REVERSE,
